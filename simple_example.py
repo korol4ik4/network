a98bb22394739f0adce77e6cc14b3e1665b78828
korol4ik4 <korol4ik@tutanota.com>
2023-04-10 12:01:54 +0200

    example

--- simple_example.py
+++ simple_example.py
@@ -9,12 +9,12 @@ cs = Server(1111,timeout=50)
 cc = Client(address="127.0.0.1",port=1111,timeout=5)
 
 # client send data
-cc.send_data('priwet2'.encode(),data_type='text' )
+cc.send_data('priwet2'.encode(),datatype='text' )
 # server send data to all connected
 for serv_con in cs.control.connect:
     cs.send_data(serv_con, 'ok'.encode(), datatype='ok')
 #client send data
-cc.send_data("close".encode(), datatype="exit")
+cc.send_data("By-by".encode(), datatype="text", sub_type="exit")
 # exit client (wait timeout, default = 120)
 cc.close()
 # exit server
