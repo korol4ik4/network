eef592e36e5a9eaf1444d049ef8126debd535a33
korol4ik4 <korol4ik@tutanota.com>
2023-04-10 12:27:22 +0200

    defaul timeout at 120 sec.

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from network_thread.network_threading import NetwokThread
 import socket
 
 class Server(NetwokThread):
-    def __init__(self, port, address ='',timeout = 25,keys_path='keys', keys_name='server_rsa'):
+    def __init__(self, port, address ='',timeout = 120,keys_path='keys', keys_name='server_rsa'):
         super(Server,self).__init__(timeout = timeout,keys_path=keys_path, keys_name=keys_name)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind((address, port))
