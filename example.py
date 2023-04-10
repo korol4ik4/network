fcb08d708bc9e22865a3d663c6d1a970e22114c9
korol4ik4 <korol4ik@tutanota.com>
2023-04-10 12:28:03 +0200

    fix and clean

--- example.py
+++ example.py
@@ -14,13 +14,14 @@ class myServer(Server):
         if msg.datatype == 'text':
             txt = data.decode()
             print("Server receive ",txt)
+            # example function resend with upper
             self.send_data(connect=connect, data=txt.upper().encode(), **msg())
 
         elif msg.datatype == 'file':
             filename = msg.filename
             with open(filename,"wb") as fn:
                 fn.write(data)
-            print("Server recv and save file: ", filename)
+            print("Server receive and save file: ", filename)
         else:
             print('unknown data type ', print(msg))
 
@@ -31,8 +32,6 @@ class myClient(Client):
 
     def incoming(self, service_message, data, connect):
         msg = Message(service_message)
-
-        datatype = msg.datatype
         if msg.datatype == 'text':
             txt = data.decode()
             print('Client received ', txt)
@@ -44,9 +43,10 @@ class myClient(Client):
         else:
             print('unknown data type ', print(msg))
 
+
 # create and connect server and client
-server = myServer(port=5555)
-client = myClient(address="127.0.0.1",port=5555,timeout=5)
+server = myServer(address="127.0.0.1", port=5555,keys_path='keys', keys_name='server_rsa' )
+client = myClient(address="127.0.0.1",port=5555,timeout=5, keys_path='keys', keys_name='client_rsa')
 
 # send text message
 client.send_data('Привет Сервер'.encode(), datatype="text", sub_type="hello")
